91e3defb52ba010da4c29e4ec735a34fe7037837
Dieter Holvoet <dieter.holvoet@gmail.com>
2019-10-16 18:45:10 +0200

    Remove Drush 8 support

--- src/Commands/WmModelCommands.php
+++ src/Commands/WmModelCommands.php
@@ -2,17 +2,26 @@
 
 namespace Drupal\wmmodel\Commands;
 
-use Drupal\wmmodel\Service\CliService;
+use Drupal\Core\State\StateInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\wmmodel\Entity\EntityTypeBundleInfo;
 use Drush\Commands\DrushCommands;
 
 class WmModelCommands extends DrushCommands
 {
-    /** @var CliService */
-    private $cliService;
+    use StringTranslationTrait;
 
-    public function __construct(CliService $cliService)
-    {
-        $this->cliService = $cliService;
+    /** @var EntityTypeBundleInfo */
+    private $bundleInfo;
+    /** @var StateInterface */
+    private $state;
+
+    public function __construct(
+        EntityTypeBundleInfo $bundleInfo,
+        StateInterface $state
+    ) {
+        $this->bundleInfo = $bundleInfo;
+        $this->state = $state;
     }
 
     /**
@@ -23,6 +32,25 @@ class WmModelCommands extends DrushCommands
      */
     public function listModels()
     {
-        $this->cliService->listModels($this->io(), 'dt');
+        $mapping = $this->state->get('wmmodel', []);
+
+        $types = [];
+        foreach ($this->bundleInfo->getAllBundleInfo() as $entityType => $bundles) {
+            foreach (array_keys($bundles) as $bundle) {
+                $types[] = "$entityType.$bundle";
+            }
+        }
+
+        sort($types);
+
+        foreach ($types as $modelKey) {
+            $map = $mapping[$modelKey] ?? '';
+
+            if (!$map) {
+                $this->io()->text($this->t('Model "@model" is not mapped.', ['@model' => $modelKey]));
+            } else {
+                $this->io()->text($this->t('Model "@model" is mapped against "@mapping".', ['@model' => $modelKey, '@mapping' => $map]));
+            }
+        }
     }
 }
