51bcead241ca63e7731b01da3829fabd5e6ad01c
Dieter Holvoet <dieter.holvoet@gmail.com>
2020-01-31 15:46:11 +0100

    Update FieldHelpers trait

--- src/Entity/Traits/FieldHelpers.php
+++ src/Entity/Traits/FieldHelpers.php
@@ -2,35 +2,114 @@
 
 namespace Drupal\wmmodel\Entity\Traits;
 
-use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface;
+use Drupal\link\Plugin\Field\FieldType\LinkItem;
 
 trait FieldHelpers
 {
-    protected function removeReference(ContentEntityInterface $entity, $fieldName, $referenceName = 'target_id')
+    protected function getDateTime(string $fieldName): ?\DateTimeInterface
     {
-        $id = $entity->id();
-        $tagReferences = $this->get($fieldName)->getValue();
-
-        // Loop over the values, but don't necessarily load them as entities
-        foreach ($tagReferences as $delta => $reference) {
-            if ($reference[$referenceName] == $id) {
-                $this->get($fieldName)->removeItem($delta);
-                return;
-            }
+        if (!$this->hasField($fieldName) || $this->get($fieldName)->isEmpty()) {
+            return null;
         }
+
+        if (!($date = $this->get($fieldName)->date) && !($date = $this->get($fieldName)->value)) {
+            return null;
+        }
+
+        if ($date instanceof DrupalDateTime) {
+            $date = $date->format('U');
+        }
+
+        $timezone = new \DateTimeZone(date_default_timezone_get());
+        $dateTime = \DateTime::createFromFormat('U', $date, $timezone);
+
+        if (!$dateTime) {
+            return null;
+        }
+
+        return $dateTime;
     }
 
-    protected function removeFromList($value, $fieldName)
+    protected function setDateTime(string $fieldName, \DateTimeInterface $dateTime): self
     {
-        if (!is_array($value)) {
-            $value = [$value];
+        $this->set($fieldName, $dateTime->format(DateTimeItemInterface::DATETIME_STORAGE_FORMAT));
+
+        return $this;
+    }
+
+    protected function formatLinks(string $fieldName): array
+    {
+        $links = [];
+
+        if (!$this->hasField($fieldName) || $this->get($fieldName)->isEmpty()) {
+            return $links;
         }
 
-        $values = array_diff(
-            array_values($this->get($fieldName)->getValue()),
-            $value
-        );
+        foreach ($this->get($fieldName) as $value) {
+            $links[] = $this->formatLinkItem($value);
+        }
 
-        $this->set($fieldName, $values);
+        return $links;
+    }
+
+    protected function formatLink(string $fieldName): array
+    {
+        $link = [
+            'url' => '',
+            'text' => '',
+            'external' => false,
+            'target' => '_self',
+        ];
+
+        /** @var LinkItem $item */
+        if (!$this->hasField($fieldName) || $this->get($fieldName)->isEmpty()) {
+            return $link;
+        }
+
+        return $this->formatLinkItem(
+            $this->get($fieldName)->first()
+        );
+    }
+
+    private function formatLinkItem(LinkItem $item): array
+    {
+        $link = [
+            'url' => '',
+            'text' => '',
+            'external' => false,
+        ];
+
+        if (!$item->isExternal() && $item->getUrl()->isRouted() && in_array($item->getUrl()->getRouteName(), ['<nolink>', '<none>'])) {
+            $url = '';
+        } elseif (!$item->isExternal() && $entity = $this->getReferencedEntityFromLink($item)) {
+            $url = $entity->toUrl()->toString();
+        } else {
+            $url = $item->getUrl()->toString();
+        }
+
+        $link['url'] = $url;
+        $link['text'] = $item->title ?? '';
+        $link['external'] = $item->isExternal();
+
+        return $link;
+    }
+
+    private function getReferencedEntityFromLink(LinkItem $link): ?EntityInterface
+    {
+        $uri = explode(':', $link->uri, 2);
+        if ($uri[0] !== 'entity' || count($uri) !== 2) {
+            return null;
+        }
+
+        [$entityTypeId, $entityId] = explode('/', $uri[1], 2);
+
+        $entity = $this->entityTypeManager()
+            ->getStorage($entityTypeId)
+            ->load($entityId);
+
+        return $this->translateEntity($entity);
     }
 }
