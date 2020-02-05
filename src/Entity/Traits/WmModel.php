7da1df5458aeb2b34e9cd47f3db62df84efa4e04
Dieter Holvoet <dieter.holvoet@gmail.com>
2020-02-05 14:51:27 +0100

    Add bundle-specific EntityInterface::loadMultiple implementation

--- src/Entity/Traits/WmModel.php
+++ src/Entity/Traits/WmModel.php
@@ -6,6 +6,35 @@ use Drupal\Core\Entity\Exception\NoCorrespondingEntityClassException;
 
 trait WmModel
 {
+    public static function loadMultiple(array $ids = null)
+    {
+        $entityTypeManager = \Drupal::entityTypeManager();
+        $modelFactory = \Drupal::service('wmmodel.factory.model');
+
+        if (!$definition = $modelFactory->getEntityTypeAndBundle(static::class)) {
+            throw new NoCorrespondingEntityClassException(static::class);
+        }
+
+        [$entityTypeId, $bundle] = $definition;
+        $entityType = $entityTypeManager->getDefinition($entityTypeId);
+        $storage = $entityTypeManager->getStorage($entityTypeId);
+
+        $query = $storage->getQuery()
+            ->condition($entityType->getKey('bundle'), $bundle);
+
+        if ($ids) {
+            $query->condition($entityType->getKey('id'), $ids, 'IN');
+        }
+
+        $ids = $query->execute();
+
+        if (empty($ids)) {
+            return [];
+        }
+
+        return $storage->loadMultiple($ids);
+    }
+
     public static function create(array $values = [])
     {
         $entityTypeManager = \Drupal::entityTypeManager();
