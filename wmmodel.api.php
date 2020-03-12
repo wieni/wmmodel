32a5c8f03a1b91f74f97a4a9d88ffda8aa720f38
Dieter Holvoet <dieter.holvoet@gmail.com>
2020-03-12 13:28:36 +0100

    #9 Update wmmodel.api.php

--- wmmodel.api.php
+++ wmmodel.api.php
@@ -1,28 +1,6 @@
 <?php
 
-/**
- * Add content types to be converted to a Model
- *
- * @return array
- */
-function hook_entity_model_mapping()
+function hook_wmmodel_model_info_alter(array &$definitions)
 {
-    $mapping = [];
-
-    // Map a bundle "article" with entity type "node" to a custom class
-    $mapping['node_article'] = 'Drupal\mymodule\Entity\Node\Article';
-
-    return $mapping;
-}
-
-/**
- * Alter existing mappings
- *
- * @param array &$mapping
- *   Array containing the mapping between entities and classes
- */
-function hook_entity_model_mapping_alter(&$mapping)
-{
-    // Map a bundle "article" with entity type "node" to a custom class
-    $mapping['node_article'] = 'Drupal\mymodule\Entity\Node\Article';
+    $definitions['node.page']['class'] = \Drupal\my_module\Entity\Model\Node\Page::class;
 }
